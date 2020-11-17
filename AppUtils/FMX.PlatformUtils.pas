eca4eadac578e3ef6e32003e818f49749fab601e
UrbanCohortDev <UrbanCohortDev@users.noreply.github.com>
2020-11-17 16:53:44 +0000

    fixed URLs in file headers. Format Source removed the //

--- AppUtils/FMX.PlatformUtils.pas
+++ AppUtils/FMX.PlatformUtils.pas
@@ -3,12 +3,12 @@ Unit FMX.PlatformUtils;
 (* ***********************************************************************
   copyright 2019-2020  Russell Weetch
   Distributed under the MIT software license, see the accompanying file
-  LICENSE or visit http:www.opensource.org/licenses/mit-license.php.
+  LICENSE or visit http://www.opensource.org/licenses/mit-license.php.
 
-  PascalCoin website http:pascalcoin.org
+  PascalCoin website http://pascalcoin.org
 
   PascalCoin Delphi RPC Client Repository
-  https:github.com/UrbanCohortDev/PascalCoin-RPC-Client
+  https://github.com/UrbanCohortDev/PascalCoin-RPC-Client
 
   PASC Donations welcome: Account (PASA) 1922-23
   BitCoin: 3DPfDtw455qby75TgL3tXTwBuHcWo4BgjL (now isn't the Pascal way easier?)
